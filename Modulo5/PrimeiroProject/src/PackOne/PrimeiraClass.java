ad209813853eb7842092438fef76d7767e2e8ff5
diniztaina <diniztainarayes@gmail.com>
2023-09-10 19:35:55 -0300

    Primeiro commit

---
+++ Modulo5/PrimeiroProject/src/PackOne/PrimeiraClass.java
@@ -0,0 +1,10 @@
+package PackOne;
+
+public class PrimeiraClass {
+
+	public static void main(String[] args) {
+		System.out.println ("Hello, Taina!");
+
+	}
+
+}
---
+++ Modulo5/PrimeiroProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rubia
+ *
+ */
+module PrimeiroProject {
+}
